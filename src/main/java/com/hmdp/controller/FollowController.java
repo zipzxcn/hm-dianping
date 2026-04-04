f327b0a73389c03e3c5869e8103584a03ad88237
zipzxcn <435392076@qq.com>
2026-04-04 16:56:43 +0800

    关注和取消关注功能

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,10 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -13,8 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 虎哥
  * @since 2021-12-22
  */
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+
+    private final IFollowService followService;
+
+    @GetMapping("/or/not/{id}")
+    public Result queryIsFollow(@PathVariable("id") Long followUserId){
+        return followService.queryIsFollow(followUserId);
+    }
+
+    @PutMapping("/{id}/{boolean}")
+    public Result followUser(@PathVariable("id") Long followUserId,@PathVariable("boolean") boolean isFollow){
+        return followService.followUser(followUserId,isFollow);
+    }
 
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,14 +1,19 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +22,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    /**
+     * 查询对用户的关注状态 已关注/未关注
+     *
+     * @return boolean
+     */
+    @Override
+    public Result queryIsFollow(Long followUserId) {
+        Long userId = UserHolder.getUser().getId();
+        Integer count = this.query().eq("user_id", userId).eq("follow_user_id", followUserId).count();
+        return Result.ok(count > 0);
+    }
+
+    /**
+     * 关注用户
+     * @param followUserId
+     * @param isFollow
+     * @return ok
+     */
+    @Override
+    public Result followUser(Long followUserId, boolean isFollow) {
+        // 获取用户信息
+        Long userId = UserHolder.getUser().getId();
+        // 判断用户是否已关注当前用户
+        if (isFollow) {
+            // 未关注，执行关注操作 保存关注信息
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followUserId);
+            save(follow);
+        }else {
+            // 已关注，执行取关操作 移除关注信息
+            LambdaQueryWrapper<Follow> queryWrapper = new LambdaQueryWrapper<>();
+            remove(queryWrapper.eq(Follow::getUserId,userId).eq(Follow::getFollowUserId,followUserId));
+        }
+        return Result.ok();
+    }
 }
